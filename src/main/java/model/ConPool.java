0ee5146f81cf6820f05d066364860c9b71ef5a39
vale <valeannunziatared2@gmail.com>
2023-02-18 12:46:56 +0100

    altri

---
+++ src/main/java/model/ConPool.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.TimeZone;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+
+
+public class ConPool {
+    private static DataSource datasource;
+
+    public ConPool() {
+    }
+/*jdbc:mysql://127.0.0.1:3306/?user=root*/
+    public static Connection getConnection() throws SQLException {
+        if (datasource == null) {
+            PoolProperties p = new PoolProperties();
+            p.setUrl("jdbc:mysql://127.0.0.1:3306/pasticceria?serverTimezone=" + TimeZone.getDefault().getID());
+            p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            p.setUsername("root");
+            p.setPassword("valentina");
+            p.setMaxActive(100);
+            p.setInitialSize(10);
+            p.setMinIdle(10);
+            p.setRemoveAbandonedTimeout(60);
+            p.setRemoveAbandoned(true);
+            datasource = new DataSource();
+            datasource.setPoolProperties(p);
+        }
+
+        return datasource.getConnection();
+    }
+}
