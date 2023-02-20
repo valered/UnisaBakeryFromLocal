3886b626c5ff5628f5f9f44aa6495e0af5cf1b48
zEmanu <92575456+zEmanu@users.noreply.github.com>
2023-02-20 12:41:17 +0100

    finale

--- src/main/java/model/ConPool.java
+++ src/main/java/model/ConPool.java
@@ -17,10 +17,10 @@ public class ConPool {
     public static Connection getConnection() throws SQLException {
         if (datasource == null) {
             PoolProperties p = new PoolProperties();
-            p.setUrl("jdbc:mysql://127.0.0.1:3306/pasticceria?serverTimezone=" + TimeZone.getDefault().getID());
+            p.setUrl("jdbc:mysql://localhost:3306/pasticceria?serverTimezone=" + TimeZone.getDefault().getID());
             p.setDriverClassName("com.mysql.cj.jdbc.Driver");
             p.setUsername("root");
-            p.setPassword("valentina");
+            p.setPassword("admin");
             p.setMaxActive(100);
             p.setInitialSize(10);
             p.setMinIdle(10);
